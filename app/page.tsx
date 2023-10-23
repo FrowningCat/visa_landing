ab52977605206dedbfb88c6c4a30455d9003b60e
FrowningCat <vsavinkov60@gmail.com>
2023-10-23 11:11:56 +0300

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import ru from '../public/RU.png';
+import chevronDownGray from '../public/chevron.down.gray.png';
+import telegram from '../public/telegram.png';
 
 export default function Home() {
     return (
         <div className="landing">
-            <header className="header"></header>
+            <header className="header">
+                <div className="header__block">
+                    <div className="header__imgFlag">
+                        <Image src={ru} alt="flag" />
+                        <Image
+                            src={chevronDownGray}
+                            alt="chevronDownGray"
+                            className="link"
+                        />
+                    </div>
+                    <div className="header__tg">
+                        <Image src={telegram} alt="telegram" />
+                        <Link href="https://t.me/Frowning_Cat">
+                            <text className="link">Чат поддержки</text>
+                        </Link>
+                    </div>
+                    <div className="header__enterText link">
+                        <text>Вход</text>
+                    </div>
+                </div>
+                <div className="header__button link">
+                    <text>Подать заявление</text>
+                </div>
+            </header>
             <main className="main"></main>
-            <footer className="footer">
-                <h1>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Sequi, minima.
-                </h1>
-            </footer>
+            <footer className="footer"></footer>
         </div>
     );
 }
