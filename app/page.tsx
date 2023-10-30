29f36fd20280a09a502abfffbac9ab6c7265a5d1
FrowningCat <91006821+FrowningCat@users.noreply.github.com>
2023-10-30 14:27:59 +0300

    big fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import ru from '../public/RU.png';
 import chevronDownGray from '../public/chevron.down.gray.png';
+import arrow from '../public/arrow.png';
 import telegram from '../public/telegram.png';
+import clipboard from '../public/clipboard.png';
+import bg from '../public/bg.png';
+import city from '../public/city.png';
+import iPhone from '../public/iPhone.png';
+import vectorTg from '../public/vectorTg.png';
 
 export default function Home() {
     return (
@@ -31,8 +37,188 @@ export default function Home() {
                     <text>Подать заявление</text>
                 </div>
             </header>
-            <main className="main"></main>
-            <footer className="footer"></footer>
+            <main className="main">
+                <div className="main__topDiv">
+                    <div className="main__topBlok">
+                        <div className="main__typeOfQuestionnaire">
+                            <div className="main__Clipboard">
+                                <Image src={clipboard} alt="clipboard" />
+                            </div>
+                            <text> Анкета DS-160</text>
+                        </div>
+                        <div className="main__topBlokTitle">
+                            Получите{' '}
+                            <text className="additionalColor">визу США</text> в
+                            срок от 1 дня
+                        </div>
+                        <div className="main__topBlokText">
+                            Разработанный ИИ-алгоритм ускорения записи и удобный
+                            интерфейс для заполнения анкеты, оплата Госпошлины с
+                            российской карты - подача документов на визу в США
+                            <br />
+                            <text className="additionalColor additionalWeight">
+                                за считанные минуты.
+                            </text>
+                        </div>
+                    </div>
+                    <div className="main__topButton link">
+                        <text>Подать заявление</text>
+                        <Image src={arrow} alt="arrow" />
+                    </div>
+                </div>
+                <Image src={bg} alt="bg" className="main__bg" />
+                <div className="main__info">
+                    <div className="main__infoTitle">
+                        <text className="main__title">
+                            Подача документов на визу США
+                        </text>
+                        <text className="main__infoText">
+                            Разработанный ИИ-алгоритм ускорения записи и удобный
+                            интерфейс для заполнения анкеты
+                        </text>
+                    </div>
+                    <div className="main__infoBlock">
+                        <div className="main__infoDiv">
+                            <div className="main__Clipboard">
+                                <Image src={clipboard} alt="clipboard" />
+                            </div>
+                            <div className="main__infoBlockTitle">EASY</div>
+                            <div className="main__infoBlockText">
+                                Простое заполнение формы DS-160 с подсказками
+                            </div>
+                        </div>
+                        <div className="main__infoDiv">
+                            <div className="main__Clipboard">
+                                <Image src={clipboard} alt="clipboard" />
+                            </div>
+                            <div className="main__infoBlockTitle">FAST</div>
+                            <div className="main__infoBlockText">
+                                Собеседование в обход долгой очереди
+                            </div>
+                        </div>
+                        <div className="main__infoDiv">
+                            <div className="main__Clipboard">
+                                <Image src={clipboard} alt="clipboard" />
+                            </div>
+                            <div className="main__infoBlockTitle">AI</div>
+                            <div className="main__infoBlockText">
+                                Разработанный ИИ-алгоритм для подачи документов
+                            </div>
+                        </div>
+                        <div className="main__infoDiv">
+                            <div className="main__Clipboard">
+                                <Image src={clipboard} alt="clipboard" />
+                            </div>
+                            <div className="main__infoBlockTitle">FLEXIBLE</div>
+                            <div className="main__infoBlockText">
+                                Оплата Госпошлины США с российской карты
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Image src={city} alt="city" className="main__city" />
+                <div className="main__foooter">
+                    <div className="main__submissionOfBocuments">
+                        <div className="main__title">Подача документов</div>
+                        <div className="main__submissionOfBocumentsStages">
+                            <div className="main__submissionOfBocumentsStage">
+                                <div className="main__stageNumber">1</div>
+                                <div className="main__stageDiv">
+                                    <div className="main__stageTitle">
+                                        Подаете документы на визу
+                                    </div>
+                                    <div className="main__stageText">
+                                        Заполняете анкету на нашем сайте.
+                                        Необходимые документы: паспорт,
+                                        загранпаспорт. Выбираете удобное
+                                        консульство для собеседования
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="main__submissionOfBocumentsStage">
+                                <div className="main__stageNumber">2</div>
+                                <div className="main__stageDiv">
+                                    <div className="main__stageTitle">
+                                        Оплачиваете
+                                    </div>
+                                    <div className="main__stageText">
+                                        Оплатить услуги можно с любой российской
+                                        карты (мир, виза, оплата через сайт)
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="main__submissionOfBocumentsStage">
+                                <div className="main__stageNumber">3</div>
+                                <div className="main__stageDiv">
+                                    <div className="main__stageTitle">
+                                        Ожидаете ответа
+                                    </div>
+                                    <div className="main__stageText">
+                                        Ожидание может занять от 10 дней. За
+                                        дополнительную плату{' '}
+                                        <text className="additionalColor additionalWeight">
+                                            можно ускорить продвижение в очереди
+                                        </text>{' '}
+                                        ($450, от 1 дня)
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="main__submissionOfBocumentsStage">
+                                <div className="main__stageNumber">4</div>
+                                <div className="main__stageDiv">
+                                    <div className="main__stageTitle">
+                                        Виза готова!
+                                    </div>
+                                    <div className="main__stageText">
+                                        Уведомление придет вам на почту, можете
+                                        смело отправляться в путешествие!
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Image src={iPhone} alt="iPhone" className="main__iPhone" />
+                    <div className="main__questions">
+                        <div className="main__title">Остались вопросы?</div>
+                        <div className="main__questionsMain">
+                            <div className="main__questionsText">
+                                Напишите нам в телеграм
+                            </div>
+                            <div className="main__questionsButton">
+                                <Image src={vectorTg} alt="vectorTg" />
+                                <text className="main__questionsButtonText">
+                                    <Link href="https://t.me/Frowning_Cat">
+                                        Чат поддержки
+                                    </Link>
+                                </text>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer className="footer">
+                <div className="footer__divText1">
+                    <Link href="https://t.me/Frowning_Cat">
+                        <text className="footer__telegram">
+                            <Image src={telegram} alt="telegram" />
+                            Чат поддержки
+                        </text>
+                    </Link>
+                    <text>© 2000-2023. Все права защищены</text>
+                    <Link href="https://t.me/Frowning_Cat">
+                        <text>Условия использования сервиса</text>
+                    </Link>
+                    <Link href="https://t.me/Frowning_Cat">
+                        <text>Политика конфиденциальности</text>
+                    </Link>
+                </div>
+                <div className="footer__divText2">
+                    <text>
+                        Сайт не является публичной офертой согласно положениям
+                        статьи 437 ГК РФ. ИП ..ОГРНИП 317505300078169
+                    </text>
+                </div>
+            </footer>
         </div>
     );
 }
