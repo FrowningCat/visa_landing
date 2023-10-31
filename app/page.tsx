c076d6ca8746bdface85c87a027408d2c8e9f041
FrowningCat <91006821+FrowningCat@users.noreply.github.com>
2023-10-31 10:43:54 +0300

    add FAQ

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import bg from '../public/bg.png';
 import city from '../public/city.png';
 import iPhone from '../public/iPhone.png';
 import vectorTg from '../public/vectorTg.png';
+import chevron from '../public/chevron.png';
 
 export default function Home() {
     return (
@@ -178,6 +179,110 @@ export default function Home() {
                         </div>
                     </div>
                     <Image src={iPhone} alt="iPhone" className="main__iPhone" />
+                    <div className="main__faq">
+                        <div className="main__title">FAQ</div>
+                        <div className="main__faqDetails">
+                            <details>
+                                <summary>
+                                    Сколько делают визу США?{' '}
+                                    <Image
+                                        src={chevron}
+                                        alt="chevron"
+                                        className="chevron"
+                                    />
+                                </summary>
+                                <div>
+                                    После приема документов на визу, консульство
+                                    берет время на их обработку и проверку. Как
+                                    правило, срок выдачи составляет около
+                                    недели. <br />В период наибольшей
+                                    загруженности инстанции этот период может
+                                    растягиваться по времени до 10-14 дней.
+                                    <br />
+                                    Сотрудничая с нами можно ускорить процесс,
+                                    доплатив за срочность. В этом случае{' '}
+                                    <text className="additionalColor">
+                                        есть шанс получить визу в течение 3 дней
+                                        или при необходимости даже в течение 1
+                                        дня.
+                                    </text>
+                                </div>
+                            </details>
+                            <details>
+                                <summary>
+                                    На сколько дают визу США?{' '}
+                                    <Image
+                                        src={chevron}
+                                        alt="chevron"
+                                        className="chevron"
+                                    />
+                                </summary>
+                                <div>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nobis pariatur
+                                    necessitatibus fugiat quae ipsam totam,
+                                    autem iusto, commodi vero mollitia ab
+                                    similique, quos consequuntur? Quam similique
+                                    velit expedita commodi accusantium?
+                                </div>
+                            </details>
+                            <details>
+                                <summary>
+                                    Что нужно для визы США?{' '}
+                                    <Image
+                                        src={chevron}
+                                        alt="chevron"
+                                        className="chevron"
+                                    />
+                                </summary>
+                                <div>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nobis pariatur
+                                    necessitatibus fugiat quae ipsam totam,
+                                    autem iusto, commodi vero mollitia ab
+                                    similique, quos consequuntur? Quam similique
+                                    velit expedita commodi accusantium?
+                                </div>
+                            </details>
+                            <details>
+                                <summary>
+                                    Размер консульского и сервисного сбора на
+                                    визу США?
+                                    <Image
+                                        src={chevron}
+                                        alt="chevron"
+                                        className="chevron"
+                                    />
+                                </summary>
+                                <div>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nobis pariatur
+                                    necessitatibus fugiat quae ipsam totam,
+                                    autem iusto, commodi vero mollitia ab
+                                    similique, quos consequuntur? Quam similique
+                                    velit expedita commodi accusantium?
+                                </div>
+                            </details>
+                            <details>
+                                <summary>
+                                    Какие типы визы бывают?{' '}
+                                    <Image
+                                        src={chevron}
+                                        alt="chevron"
+                                        className="chevron"
+                                    />
+                                </summary>
+                                <div>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nobis pariatur
+                                    necessitatibus fugiat quae ipsam totam,
+                                    autem iusto, commodi vero mollitia ab
+                                    similique, quos consequuntur? Quam similique
+                                    velit expedita commodi accusantium?
+                                </div>
+                            </details>
+                        </div>
+                    </div>
                     <div className="main__questions">
                         <div className="main__title">Остались вопросы?</div>
                         <div className="main__questionsMain">
